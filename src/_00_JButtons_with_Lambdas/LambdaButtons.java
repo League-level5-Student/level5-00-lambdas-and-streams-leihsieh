6ceaef3e9a4af776ca22e79a2346d25b3515bf3a
leihs <leihs@DESKTOP-D5U9TN8.lan>
2022-08-31 18:33:23 -0700

    Level 5 Mod 0 8/31/2022

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,15 +1,28 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class LambdaButtons {
+public class LambdaButtons implements ActionListener {
 	private JFrame window = new JFrame();
 	private JButton addNumbers = new JButton("ADD 2 Numbers");
 	private JButton randNumber = new JButton("RANDOM NUMBER");
 	private JButton tellAJoke = new JButton("TELL A JOKE");
+	private JPanel panel = new JPanel();
+	private JTextField field1 = new JTextField();
+	private JTextField field2 = new JTextField();
+	private JTextField answerField = new JTextField();
+	Random rand = new Random();
+	private ArrayList<String> jokes = new ArrayList<String>();
 	
 	public LambdaButtons() {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -17,10 +30,28 @@ public class LambdaButtons {
 		window.add(addNumbers);
 		window.add(randNumber);
 		window.add(tellAJoke);
+		field1.setColumns(4);
+		field2.setColumns(4);
+		answerField.setColumns(4);
+		window.add(field1);
+		window.add(field2);
+		window.add(answerField);
+		
+		jokes.add("What's the best thing about Switzerland? I don't know, but the flag is a big plus.");
+		jokes.add("I invented a new word: plagiarism.");
+		jokes.add("A bear walks into a bar and says, “Give me a whiskey… and a cola.”\n“Why the big pause?” asks the bartender. The bear shrugged. “I’m not sure. I was born with them.”");
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
-		
+		addNumbers.addActionListener((ActionEvent e)->{
+			answerField.setText("" + (Integer.parseInt(field1.getText()) + Integer.parseInt(field2.getText()))); 
+		});
+		randNumber.addActionListener((ActionEvent e)->{
+			answerField.setText("" + rand.nextInt(11));
+		});
+		tellAJoke.addActionListener((ActionEvent e)->{
+			JOptionPane.showMessageDialog(null, jokes.get(rand.nextInt(jokes.size())));
+		});
 		window.setVisible(true);
 		window.pack();
 	}
@@ -28,6 +59,10 @@ public class LambdaButtons {
 	public static void main(String[] args) {
 		new LambdaButtons();
 	}
-	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+	}
 	
 }
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -11,15 +11,45 @@ public class LambdaMethods {
 				System.out.println(s);
 			}
 		}, "repeat");
-		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
+		printCustomMessage((s)->{
+			for(int i = s.length()-1; i >= 0; i--) {
+				System.out.print(s.charAt(i));
+			}
+		}, "backwards");
+		System.out.println();
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
+		printCustomMessage((s)->{
+			for(int i = 0; i < s.length(); i++) {
+				if(i%2==0) {
+					System.out.print(Character.toUpperCase(s.charAt(i)));
+				}
+				else {
+					System.out.print(s.charAt(i));
+				}
+			}
+		}, "capitalize");
+		System.out.println();
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((s)->{
+			for(int i = 0; i < s.length(); i++) {
+				System.out.print(s.charAt(i));
+				System.out.print(".");
+			}
+		}, "periods");
+		System.out.println();
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		String vowels = "aeiou";
+		printCustomMessage((s)->{
+			for(int i = 0; i < s.length(); i++) {
+				for(char vowel : vowels.toCharArray()) {
+					if(s.charAt(i) == vowel) {
+						break;
+					}
+					
+				}
+			}
+		}, "no vowels");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
